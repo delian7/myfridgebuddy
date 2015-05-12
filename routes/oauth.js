e4e768e9271bd3612c9c540a33998c956928fe3b
Saikripa Mukund <saikripamukund@gmail.com>
2015-05-11 21:17:30 -0700

    add oauth.js

---
+++ routes/oauth.js
@@ -0,0 +1,10 @@
+var ids = {
+	facebook : {
+		clientID: '1572522816341345';
+		clientSecret: '92431a90307b7b14ffd2952c38afe978';
+		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	}
+
+}
+
+module.exports = ids;
